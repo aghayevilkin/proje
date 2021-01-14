68cf615ad8254db4bc61a7f81af8a51f0b54a5d4
ilkin <ilkin200106@gmail.com>
2021-01-14 12:47:08 +0400

    Test

---
+++ Homework/script.js
@@ -0,0 +1,9 @@
+const navToggle = document.querySelector(".nav-toggle");
+const links = document.querySelector(".links");
+
+navToggle.addEventListener("click", () => {
+    console.log(links.classList.contains("show-links"));
+    links.classList.toggle("show-links");
+});
+
+window.addEventListener("resize", () => links.classList.remove("show-links"));
